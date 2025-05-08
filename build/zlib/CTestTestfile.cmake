38ed408c79af148662e09578e37abf2d4ecfcd04
pawanhardikar <pawanhardikar@gmail.com>
2025-05-08 19:33:43 +0530

    Added gitingore and linked zlib directly instead of having it compiled in my project

--- build/zlib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/zlib_project/zlib
-# Build directory: D:/zlib_project/build/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
